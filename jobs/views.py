fc69b262d824c2262e5f2536ec1079b25eb08dc3
Tobi <Adeniyisoftware@gmail.com>
2021-03-01 14:24:28 +0000

    FEAT: Register Jobs

--- jobs/views.py
+++ jobs/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import Job
 
 # Create your views here.
+
+
+def homepage(request):
+    jobs = Job.objects.all()
+    response = {'jobs': jobs}
+    return render(request, 'jobs/home.html', response)
