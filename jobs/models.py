ec599e2e9c1c38044754fee47e66b86eafeb3076
Tobi <Adeniyisoftware@gmail.com>
2021-03-04 14:43:02 +0000

    FEAT: Add github_url to Job model

--- jobs/models.py
+++ jobs/models.py
@@ -7,6 +7,7 @@ class Job(models.Model):
     title = models.CharField(max_length=50)
     image = models.ImageField(upload_to='images/')
     summary = models.CharField(max_length=200)
+    github_url = models.URLField(max_length=200)
 
     def __str__(self):
         return self.title
